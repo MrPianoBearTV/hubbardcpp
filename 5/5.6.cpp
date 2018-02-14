7fec9a0b8b4128e414230014689735b199d3312f
Carlos Vaz <carjorvaz@gmail.com>
2018-02-14 11:35:06 +0000

    5.6. Prática

---
+++ 5/5.6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int min(int, int, int, int);
+
+int main() {
+  int a, b, c, d;
+  cout << "Escreve 4 números" << endl;
+  cin >> a >> b >> c >> d;
+  cout << "O menor número é " << min(a, b, c, d) << endl;
+}
+
+int min(int a, int b, int c, int d) {
+  int min = a;
+
+  if (min > b) {
+    min = b;
+  }
+
+  if (min > c) {
+    min = c;
+  }
+
+  if (min > d) {
+    min = d;
+  }
+}
