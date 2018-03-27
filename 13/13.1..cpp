6b4616f480957aaf90d536f11b74bd0c1b849db1
Carlos Vaz <carjorvaz@gmail.com>
2018-03-27 18:50:20 +0100

    13.1. Prática - outro exemplo

--- 13/13.1..cpp
+++ 13/13.1..cpp
@@ -16,5 +16,9 @@ int main() {
 
   cout << "Between " << x << " and " << y << ", " << min(x, y) << " is the smallest." << endl;
 
+  int a = 20, b = 10;
+
+  cout << "Between " << a << " and " << b << ", " << min(a, b) << " is the smallest." << endl;
+
   return 0;
 }
